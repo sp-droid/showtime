17d682e42fb7935ac94ad75787da26d2f977f4f1
sp-droid <52839915+sp-droid@users.noreply.github.com>
2022-10-30 10:52:23 +0100

    Update star.js

--- pages/2TCT/1starfield/star.js
+++ pages/2TCT/1starfield/star.js
@@ -1,7 +1,7 @@
 function Star() {
     const farsize = 0;
     const closesize = 2;
-    const vz = 20;
+    const vz = 5;
     const depth = 2000;
 
     [this.x, this.y, this.z, this.pz] = initStar();
