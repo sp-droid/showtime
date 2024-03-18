46346332e84780c4c4f4c19db00e75a1c79ff305
sp-droid <52839915+sp-droid@users.noreply.github.com>
2024-03-18 15:10:19 +0100

    Replaced discord with flickr

--- js/topbar.js
+++ js/topbar.js
@@ -6,10 +6,11 @@ document.addEventListener("DOMContentLoaded", function() {
     <div class="row" id="linksTopbar">
         <div class="col-10 d-flex" style="align-items: center; background-color: rgb(17, 75, 95); color: white; height: 100%;">
             &emsp;<a href="about.html" style="text-decoration: none; color: white;" title="Who am I?">Arbelo Cabrera, Pablo</a>
-            &emsp;<a class="linkPopUp" href="https://www.youtube.com/@pactomars"><i class="fa-brands fa-youtube fa-lg" title="My YouTube channel" style="color: rgb(255, 129, 129)"></i></a>
-            &emsp;<a class="linkPopUp" href="https://discord.gg/S2FQRHu9JX"><i class="fa-brands fa-discord fa-lg" title="The discord server for development of the game I'm sporadically working on" style="color: rgb(83, 95, 238)"></i></a>
-            &emsp;<a class="linkPopUp" href="https://www.linkedin.com/in/pablo-arbelo-cabrera-051a951a2"><i class="fa-brands fa-linkedin-in fa-lg" title="My LinkedIn" style="color: rgb(90, 176, 247)"></i></a>
-            &emsp;<a class="linkPopUp" href="https://github.com/sp-droid"><i class="fa-brands fa-github fa-lg" title="My GitHub profile, check it out!" style="color: rgb(230, 237, 243)"></i></a>
+            &emsp;<a class="linkPopUp" href="https://www.youtube.com/@pactomars"><i class="fa-brands fa-youtube fa-lg" title="YouTube channel" style="color: rgb(255, 129, 129)"></i></a>
+            <!-- &emsp;<a class="linkPopUp" href="https://discord.gg/S2FQRHu9JX"><i class="fa-brands fa-discord fa-lg" title="The discord server for development of the game I'm sporadically working on" style="color: rgb(83, 95, 238)"></i></a> -->
+            &emsp;<a class="linkPopUp" href="https://www.flickr.com/photos/200282744@N03/albums" title="flickr albums"><img height="45em" src="../img/icons/flickr.svg" alt="flickr official logo."></a>
+            &emsp;<a class="linkPopUp" href="https://www.linkedin.com/in/pablo-arbelo-cabrera-051a951a2"><i class="fa-brands fa-linkedin-in fa-lg" title="LinkedIn profile" style="color: rgb(90, 176, 247)"></i></a>
+            &emsp;<a class="linkPopUp" href="https://github.com/sp-droid"><i class="fa-brands fa-github fa-lg" title="GitHub profile" style="color: rgb(230, 237, 243)"></i></a>
             <span class="longScreen">&emsp;This website is under construction</span>
         </div>
         <div class="col d-flex" style="align-items: center; justify-content: center; background-color: rgb(253, 232, 170); color: rgb(64,74,61); height: 100%;">
