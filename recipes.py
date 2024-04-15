9f00c6b43926df3b3912cc4774946eb1af6c7b82
sp-droid <52839915+sp-droid@users.noreply.github.com>
2024-04-15 22:01:30 +0200

    Many new recipes and improvements

--- recipes.py
+++ recipes.py
@@ -5,25 +5,40 @@ def categoryIcon(category):
     if category == "Appetizers": icon = None
     elif category == "Main courses": icon = '<i class="fas fa-drumstick-bite"></i>'
     elif category == "Desserts": icon = '<i class="fas fa-ice-cream"></i>'
-    elif category == "Other": icon = None
+    elif category == "Other": icon = '<i class="fas fa-lemon"></i>'
     else: raise ValueError(f"Category {category} not found.")
     return icon
 
 foodProperties = { # Per 100g, calories / fat / carbohydrates / sugar / protein
-    "egg":          [       142,        9.96,       0.96,       0.2,        12.4],
-    "egg yolk":     [       322,        26.5,       3.59,       0.56,       15.9],
-    "mascarpone":   [       429,        50,         0,          0,          7.14],
-    "savoiardi":    [       367,        3,          77,         40,         7],
-    "sugar":        [       385,        0.32,       99.7,       99.7,       0]
+    "bell pepper":      [   32,     0.16,   6.7,    4.2,    0.88],
+    "carrot":           [   48,     0.35,   10.3,   4.7,    0.94],
+    "chicken breast":   [   106,    1.93,   0,      0,      22.5],
+    "egg":              [   142,    9.96,   0.96,   0.2,    12.4],
+    "egg yolk":         [   322,    26.5,   3.59,   0.56,   15.9],
+    "garlic":           [   143,    0.38,   28.2,   1,      6.62],
+    "heavy cream":      [   340,    36.1,   2.9,    2.9,    2.84],
+    "mascarpone":       [   429,    50,     0,      0,      7.14],
+    "milk":             [   61,     3.2,    4.9,    4.9,    3.27],
+    "onion":            [   36,     0.13,   7.68,   5.76,   0.89],
+    "pasta":            [   371,    1.51,   74.67,  2.7,    13.04],
+    "rice":             [   359,    1.3,    79.8,   0,      6.94],
+    "savoiardi":        [   367,    3,      77,     40,     7],
+    "sugar":            [   385,    0.32,   99.7,   99.7,   0],
+    "tomato":           [   18,     0.2,    3.9,    2.6,    0.9]
 }
 specialFoods = { # Weigh of each e.g. egg in grams
+    "bell pepper": 120,
+    "carrot": 61,
     "egg": 58,
     "egg yolk": 15,
-    "savoiardi": 10
+    "garlic": 5,
+    "onion": 160,
+    "savoiardi": 10,
+    "tomato": 75
 }
 
-def getNutrition(ingredient, nutrition):
-    ingredient = ingredient.lower().split("(")[0]
+def getNutrition(string, nutrition):
+    ingredient = string.lower().split("(")[0]
     if "tsp" in ingredient or "tbsp" in ingredient: return nutrition
     quantity = ''.join(filter(str.isdigit, ingredient))
     if quantity == '': return nutrition
@@ -33,8 +48,11 @@ def getNutrition(ingredient, nutrition):
     factor = quantity/100
     if ingredient in specialFoods: factor *= specialFoods[ingredient]
 
-    nutrition[ingredient] = [elem*factor for elem in foodProperties[ingredient]]
-    print(ingredient)
+    try:
+        nutrition[ingredient] = [elem*factor for elem in foodProperties[ingredient]]
+    except:
+        raise ValueError(string)
+    
     return nutrition
     
 
@@ -46,6 +64,7 @@ with open(f"templates/recipes/template.html", "r") as file:
 # List existing recipes
 recipes = Path("content/recipes").glob("*")
 
+recipeRows = ""
 # Load each file, edit the template accordingly and save as a new html
 for recipePath in recipes:
     with open(recipePath, "r", encoding="utf-8") as file:
@@ -68,7 +87,7 @@ for recipePath in recipes:
     content = content.replace("{{portions}}", str(recipe["portions"]))
 
     ingredients = ""
-    nutrition = {}
+    nutrition = {"dummy": [0,0,0,0,0]}
     for group in recipe["ingredients"]:
         ingredients += f"<h4>{group}</h4><ul>"
         for ingredient in recipe["ingredients"][group]:
@@ -89,10 +108,13 @@ for recipePath in recipes:
         elif line[0] == "#":
             instructions += f"<h4>{line[2:]}</h4>"
         elif line[0] == "-":
+            if i==0: instructions += "<ol>"
             if recipe["instructions"][i-1][0] != "-":
                 instructions += "<ol>"
             instructions += f"<li>{line[2:]}</li>"
-            if recipe["instructions"][i+1] == "":
+            if len(recipe["instructions"])==i+1:
+                instructions += "</ol>"
+            elif recipe["instructions"][i+1] == "":
                 instructions += "</ol>"
             elif recipe["instructions"][i+1][0] != "-":
                 instructions += "</ol>"
@@ -100,7 +122,12 @@ for recipePath in recipes:
             instructions += f'<p style="margin-bottom: 12px;">{line}</p>'
     content = content.replace("{{instructions}}", instructions)
 
-    content = content.replace("{{variants}}", "\n".join(str(elem) for elem in recipe["variants"]))
+    variants = ""
+    if len(recipe["variants"]) > 0:
+        variants += "<ul><li>"
+        variants += "</li><li>".join(str(elem) for elem in recipe["variants"])
+        variants += "</li></ul>"
+    content = content.replace("{{variants}}", variants)
 
     nutritionKcal = int(sum([nutrition[key][0] for key in nutrition])/recipe["portions"])
     content = content.replace("{{nutritionKcal}}", str(nutritionKcal))
@@ -134,3 +161,13 @@ for recipePath in recipes:
 
     with open(f"templates/recipes/{recipePath.stem}.html", "w", encoding="utf-8") as file:
         file.write(content)
+
+    recipeRows += "<tr><td>"
+    recipeRows += f'<a style="text-decoration: none;" href="recipes/{recipePath.stem}.html">{recipe["name"]}</a>'
+    recipeRows += f'</td><td>{recipe["flags"]["cuisine"]}</td></tr>'
+
+with open("templates/recipes/templateIndex.html", "r", encoding="utf-8") as file:
+    content = file.read()
+content = content.replace("{{recipeRows}}", recipeRows)
+with open(f"templates/recipes.html", "w", encoding="utf-8") as file:
+    file.write(content)
