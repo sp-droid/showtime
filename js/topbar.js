e6b5974d535fcc1d118b05f65df8224bf592115d
sp-droid <52839915+sp-droid@users.noreply.github.com>
2024-09-04 21:03:42 +0200

    More aesthetic improvements

--- js/topbar.js
+++ js/topbar.js
@@ -20,17 +20,16 @@ document.addEventListener("DOMContentLoaded", function() {
             <span class="longScreen" style="font-family: brushScriptMTfont;">&emsp;This website is <u>work in progress</u></span>
         </div>
         <div style="flex: 1; display: flex; align-items: center; justify-content: center; background-color: rgb(76, 57, 2); height: 100%;">
-            <a class="linkPopUp longScreen" title="For work or other topics" href="https://docs.google.com/forms/d/1pK5hPSywf1resOSLIUOESFpqQn6cnRa2YjkRF7d77D8" style="font-weight: 500; color: rgb(222, 237, 240)">Contact link</a>
-            <a class="linkPopUp shortScreen" title="For work or other topics" href="https://docs.google.com/forms/d/1pK5hPSywf1resOSLIUOESFpqQn6cnRa2YjkRF7d77D8" style="font-weight: 500; color: rgb(222, 237, 240)">Contact</a>
+            <a class="linkPopUp" title="For work or other topics" href="https://docs.google.com/forms/d/1pK5hPSywf1resOSLIUOESFpqQn6cnRa2YjkRF7d77D8" style="font-weight: 500; color: rgb(222, 237, 240)">Contact</a>
         </div>
     </div>
     <div class="row" id="navTopbar" style="background: linear-gradient(to right, rgb(48, 40, 2), rgb(35, 38, 39))">
-        <div class="col d-flex align-items-center justify-content-center" style="line-height: 20px; height: 100%">
-            <!-- <a id="indexMenuButton" href="${rootFolder}templates/index.html" title="Homepage where recent or current projects/entries/news are showcased" style="text-decoration: none; color: rgb(202, 197, 190);">Home</a>&emsp; -->
-            <a id="projectsMenuButton" href="${rootFolder}templates/projects.html" title="Projects of different kinds are highlighted, sometimes as a video, as a JS browser-app or as an entire webpage" style="text-decoration: none; color: rgb(202, 197, 190);">Projects</a>&emsp;
-            <a id="recipesMenuButton" href="${rootFolder}templates/recipes.html" title="Some of my recipes since I like cooking quite a bit" style="text-decoration: none; color: rgb(202, 197, 190);">Recipes</a>&emsp;
-            <a id="blogMenuButton" href="${rootFolder}templates/blog.html" title="Devlogs and personal notes" style="text-decoration: none; color: rgba(202, 197, 190);">Blog</a>&emsp;
-            <a id="aboutMenuButton" href="${rootFolder}templates/about.html" title="Brief page about myself for anyone interested" style="text-decoration: none; color: rgb(202, 197, 190);">About me</a>&emsp;
+        <div class="col d-flex align-items-center justify-content-center" style="line-height: 20px; height: 100%;">
+            <!-- <a id="indexMenuButton" href="${rootFolder}templates/index.html" title="Homepage where recent or current projects/entries/news are showcased">Home</a> -->
+            <a id="projectsMenuButton" href="${rootFolder}templates/projects.html" title="Projects of different kinds are highlighted, sometimes as a video, as a JS browser-app or as an entire webpage">Projects</a>
+            <a id="recipesMenuButton" href="${rootFolder}templates/recipes.html" title="Some of my recipes since I like cooking quite a bit">Recipes</a>
+            <a id="blogMenuButton" href="${rootFolder}templates/blog.html" title="Devlogs and personal notes">Blog</a>
+            <a id="aboutMenuButton" href="${rootFolder}templates/about.html" title="Brief page about myself for anyone interested">About me</a>
         </div>
     </div>`;
     HTMLtopbar.innerHTML = topbarContent;
@@ -38,6 +37,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Bolden required button
     const boldenButtonName = `${section}MenuButton`;
     const boldenButton = document.getElementById(boldenButtonName);
-    boldenButton.style.fontWeight = 'bolder';
+    boldenButton.style.color = 'rgb(255, 230, 255)';
+    boldenButton.style.fontWeight = '700';
     
 });
