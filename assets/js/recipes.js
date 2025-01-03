a79fa54364be221bbfdb0623bf68a8cb8f452ca7
sp-droid <52839915+sp-droid@users.noreply.github.com>
2025-01-03 12:42:34 +0100

    Recipe grid improvements

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -57,26 +57,6 @@ document.addEventListener("DOMContentLoaded", function() {
     checkboxVegetarian.addEventListener('click', function() { orderData(-1); })
     checkboxVegan.addEventListener('click', function() { orderData(-1); })
     checkboxIncomplete.addEventListener('click', function() { orderData(-1); })
-    orderData(0);
-    selectRecipe(selected);
-
-    const gridRecipes = document.getElementById("gridRecipes");
-    populateGrid();
-    const recipeIndexTable = document.getElementById("recipeIndexTable");
-    const recipeIndexGrid = document.getElementById("recipeIndexGrid");
-    const buttonSwitch = document.getElementById("buttonSwitch");
-    let switchState = 1;
-    buttonSwitch.addEventListener('click', function() {
-        if (switchState === 1) {
-            recipeIndexGrid.style.display = 'none'
-            recipeIndexTable.style.display = 'flex'
-        } else {
-            recipeIndexTable.style.display = 'none'
-            recipeIndexGrid.style.display = 'flex'
-        }
-        switchState *= -1;
-    })
-
     buttonPrevRecipe.addEventListener('click', function() {
         prevSelected = selected;
         selected -= 1;
@@ -103,99 +83,27 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    function populateGrid() {
-        // const j = 0;
-        // const i = filteredOrder[j];
-        // tableRecipesBody.children[prevSelected].children[0].style.fontWeight = 'normal';
-
-        // imageRecipe.style.opacity = 0;
-        // setTimeout(() => {
-        //     imageRecipe.src = `../assets/img/recipes/${data[i]['file']}.jpg`;
-        //     imageRecipe.onload = () => {
-        //         imageRecipe.style.opacity = 1;
-        //     };
-        //     imageRecipe.title = data[i]['description'];
-        // }, 200);
-        // titleRecipe.children[0].textContent = data[i]['name']
-        // titleRecipe.href = `recipes/${data[i]['file']}.html`;
-        // flagDifficulty.children[0].src = `../assets/img/icons/difficulty${data[i]['difficulty']}.png`;
-        // flagDifficulty.children[1].textContent = data[i]['difficulty'];
-        // flagCuisine.children[0].src = `../assets/img/icons/cuisine${data[i]['cuisine']}.png`;
-        // flagCuisine.children[1].textContent = data[i]['cuisine'];
-        // flagCuisine.title = data[i]['origin'];
-        // flagPrepTime.textContent = data[i]['prepTime'];
-        // flagTotalTime.textContent = data[i]['time'];
-
-        // tableRecipesBody.children[selected].children[0].style.fontWeight = 'bolder';
-
-        for (let j = 0; j < filteredOrder.length; j++) {
-            const i = filteredOrder[j];
-            console.log(`../assets/img/recipes/${data[i]['file']}.jpg')`)
-            const gridElement = document.createElement("a");
-            gridElement.classList.add("gridRecipe");
-            //gridElement.setAttribute("target", "_blank");
-            //gridElement.setAttribute("rel", "noopener noreferrer");
-            
-            gridElement.href = `recipes/${data[i]['file']}.html`;
-            gridElement.style.backgroundImage = `url('../assets/img/recipes/${data[i]['file']}.jpg')`;
-            
-            // const innerDiv = document.createElement("div");
-            // // Header
-            // const header = document.createElement("h3");
-            // header.innerHTML = data[i]["header"];
-            // innerDiv.appendChild(header);
-            // // Description
-            // const desc = document.createElement("p");
-            // desc.classList.add("pRich")
-            // desc.innerHTML = "<br>"+data[i]["desc"];
-            // innerDiv.appendChild(desc);
-            // // Date
-            // const date = document.createElement("h5");
-            // date.innerHTML = formatDate(data[i]["date"]);
-            // date.setAttribute("title","The assigned date could be related to the latest major change to the project or an official date of termination");
-            // innerDiv.appendChild(date);
-            // // Project type icon
-            // const projectType = document.createElement("div");
-            // const projectTypeImage = document.createElement("img");
-            // projectTypeImage.src = `../assets/img/icons/${data[i]["type"]}.png`;
-            // projectTypeImage.setAttribute("title", `Project type: ${data[i]["type"]}`);
-            // projectType.appendChild(projectTypeImage);
-            // innerDiv.appendChild(projectType);
-            // // Tools used
-            // const tools = document.createElement("div");
-            // for (const toolName of data[i]["tools"]) {
-            //     const toolImage = document.createElement("img");
-            //     toolImage.src = `../assets/img/icons/${toolName}.svg`;
-            //     toolImage.setAttribute("title", `Tool employed: ${toolName}`);
-            //     tools.appendChild(toolImage);
-            // }
-            // innerDiv.appendChild(tools);
-            // gridElement.appendChild(innerDiv);
-            
-            // // On hover GIF
-            // if (data[i]["gif"] === true) {
-            //     gridElement.addEventListener('mouseenter', function() {
-            //         gridElement.style.backgroundImage = `url('../assets/img/projects/preview/${data[i]["bg"]}.avif')`;
-            //     });
-            //     gridElement.addEventListener('mouseleave', function() {
-            //         gridElement.style.backgroundImage = `url('../assets/img/projects/preview/${data[i]["bg"]}.jpg')`;
-            //     });
-            // };
-
-            // // Importance feature
-            // if (data[i]["importance"] === 1) {
-            //     gridElement.style.border = "1px solid";
-            //     gridElement.setAttribute("title","This project is important, taking days/weeks to complete.");
-            // } else if (data[i]["importance"] === 2) {
-            //     gridElement.style.border = "2px solid gold";
-            //     gridElement.setAttribute("title","This project is very important, taking months to complete.");
-            // } else if (data[i]["importance"] === 3) {
-            //     gridElement.style.border = "3px solid darkred";
-            //     gridElement.setAttribute("title","This is a major venture, taking years to complete.");
-            // }
-            gridRecipes.appendChild(gridElement);
+    const gridRecipes = document.getElementById("gridRecipes");
+    const recipeIndexTable = document.getElementById("recipeIndexTable");
+    const recipeIndexGrid = document.getElementById("recipeIndexGrid");
+    const buttonSwitch = document.getElementById("buttonSwitch");
+    let switchState = 1;
+    buttonSwitch.addEventListener('click', function() {
+        if (switchState === 1) {
+            recipeIndexGrid.style.display = 'none'
+            recipeIndexTable.style.display = 'flex'
+        } else {
+            recipeIndexTable.style.display = 'none'
+            recipeIndexGrid.style.display = 'flex'
         }
-    }
+        switchState *= -1;
+    })
+
+    orderData(0); // Initialize 
+    order = shuffleArray(order); // Random shuffle
+    orderData(-1);
+    selectRecipe(selected);
+
     function orderData(column) {
         // Order according to clicked column
         if (column != -1) {
@@ -260,10 +168,36 @@ document.addEventListener("DOMContentLoaded", function() {
         });
 
         fillTable();
+        populateGrid(filteredOrder);
+    }
+    
+    function populateGrid(gridOrder) {
+        gridRecipes.innerHTML = '';
+        for (let j = 0; j < gridOrder.length; j++) {
+            const i = gridOrder[j];
+            const gridRecipe = document.createElement("a");
+            gridRecipe.classList.add("gridRecipe");
+            
+            gridRecipe.href = `recipes/${data[i]['file']}.html`;
+            gridRecipe.style.backgroundImage = `url('../assets/img/recipes/${data[i]['file']}.jpg')`;
+            gridRecipe.title = data[i]['description'];
 
-        // selected = 0;
-        // prevSelected = 0;
-        // selectRecipe(selected);
+            const recipeTitle = document.createElement("h3");
+            recipeTitle.textContent = data[i]['name'];
+            gridRecipe.appendChild(recipeTitle);
+            
+            gridRecipes.appendChild(gridRecipe);
+        }
+    }
+
+    function shuffleArray(array) {
+        for (let i=0; i<array.length; i++) {
+            // Generate a random index between 0 and i
+            const randomIndex = Math.floor(Math.random() * (i + 1));
+            // Swap the elements at i and randomIndex
+            [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+        }
+        return array;
     }
 
     function selectRecipe(j) {
@@ -290,7 +224,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
         tableRecipesBody.children[selected].children[0].style.fontWeight = 'bolder';
     }
-
+    
     function fillTable() {
         tableRecipesBody.innerHTML = '';
         for (let j = 0; j < filteredOrder.length; j++) {
